0401fde60253e925d6bc282eab64689c8bb4e78e
SpeagleYao <1065483675@qq.com>
2021-05-21 05:04:29 -0400

    .

--- attack.sh
+++ attack.sh
@@ -11,9 +11,9 @@
 # - ship : 8
 # - truck : 9
 
-for cst1 in `echo 1`
+for cst1 in `echo 3`
 do
-    for cst2 in `echo 9`
+    for cst2 in `echo 8`
     do
         python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_natural.pth' --target1 ${cst1} --target2 ${cst2} >> test_result.txt
         python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_normal.pth'  --target1 ${cst1} --target2 ${cst2} >> test_result.txt
