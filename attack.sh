5e36d26c7586651143d153de2976903fd96a5cb7
SpeagleYao <1065483675@qq.com>
2021-05-26 07:55:26 -0400

    result_02

--- attack.sh
+++ attack.sh
@@ -11,17 +11,17 @@
 # - ship : 8
 # - truck : 9
 
-for cst1 in `echo 1`
+for cst1 in `echo 0`
 do
-    for cst2 in `echo 9`
+    for cst2 in `echo 2`
     do
-        python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_natural.pth' --target1 ${cst1} --target2 ${cst2} >> tresult_19.txt
-        python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_normal.pth'  --target1 ${cst1} --target2 ${cst2} >> tresult_19.txt
-        python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_trades.pth'  --target1 ${cst1} --target2 ${cst2} >> tresult_19.txt
-        python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_alltar.pth'  --target1 ${cst1} --target2 ${cst2} >> tresult_19.txt
-        python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_sp_19.pth'   --target1 ${cst1} --target2 ${cst2} >> tresult_19.txt
+        python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_natural.pth' --target1 ${cst1} --target2 ${cst2} >> tresult_02.txt
+        python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_normal.pth'  --target1 ${cst1} --target2 ${cst2} >> tresult_02.txt
+        python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_trades.pth'  --target1 ${cst1} --target2 ${cst2} >> tresult_02.txt
+        python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_alltar.pth'  --target1 ${cst1} --target2 ${cst2} >> tresult_02.txt
+        python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_sp_02.pth'   --target1 ${cst1} --target2 ${cst2} >> tresult_02.txt
         # python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_sp_53.pth'   --target1 ${cst1} --target2 ${cst2} >> test_result.txt
-        python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_ow_19.pth'   --target1 ${cst1} --target2 ${cst2} >> tresult_19.txt
-        python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_ow_91.pth'   --target1 ${cst1} --target2 ${cst2} >> tresult_19.txt
+        python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_ow_02.pth'   --target1 ${cst1} --target2 ${cst2} >> tresult_02.txt
+        python attack_targeted_cifar10.py --model-path './cp_cifar10/res18_ow_20.pth'   --target1 ${cst1} --target2 ${cst2} >> tresult_02.txt
     done
 done
